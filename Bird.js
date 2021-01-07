72f7ea794cb2e3eb6130b8e9485a0120ccebf66c
Rambh71 <73946122+Rambh71@users.noreply.github.com>
2021-01-07 18:56:56 +0530

    Add files via upload

--- Bird.js
+++ Bird.js
@@ -1,27 +1,28 @@
-class Bird{
+class Bird extends BaseClass{
     constructor(x,y){
+        super(x,y,50,50);
+        this.image=loadImage("sprites/bird.png"); 
+        this.image2=loadImage("sprites/smoke.png");
+        this.trajectory=[];
+   } 
 
-        var home={
-            restitution:1
-        }
+   display(){
+      
+       super.display();
 
-        this.body = Bodies.rectangle(x,y,50,50,home);
-        this.width = 50;
-        this.height = 50;
-        World.add(myWorld,this.body);
-    }
+       if(this.body.velocity.x>10 && this.body.position.x>200){
+    
+     var position=[this.body.position.x,this.body.position.y];
+     this.trajectory.push(position);
+       }
 
-    display(){
-        var pos = this.body.position;
-        pos.x=mouseX;
-        pos.y=mouseY;
-        var angle=this.body.angle;
-        push();
-        translate(pos.x,pos.y);
-        rotate(angle);
-        fill("red");
-        rectMode(CENTER);
-        rect(0,0,this.width,this.height);
-        pop();
-    }
-} 
+     for (var i = 0; i <this.trajectory.length; i++){
+         image(this.image2,this.trajectory[i][0],this.trajectory[i][1]);
+          
+     }
+
+   }
+
+   
+    
+ }
--- sketch.js
+++ sketch.js
@@ -1,9 +1,16 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var myEngine, myWorld;
-var box,box2;
+var box,box2,bg,back,score=0;
+var gameState="onSling";
+
+function preload(){
+    getTime(); 
+      
+}
 
 function setup(){
     createCanvas(1200,600);
@@ -14,20 +21,91 @@ function setup(){
     box = new Box(800,560);
     box2=new Box(1000,560);
     pig=new Pig(900,560);
-    bird=new Bird(100,100);
+    log1=new Log(900,510,300,PI/2);
     
+    box3=new Box(800,460);
+    box4=new Box(1000,460);
+    pig2=new Pig(900,460);
+    log2=new Log(900,410,300,PI/2);
 
+   box5=new Box(900,360);
+   log3=new Log(800,300,150,PI/7);
+   log4=new Log(1000,300,150,-PI/7);
+
+    bird=new Bird(100,100);
     ground=new Ground(600,590,1200,20);
-    
+    platform=new Ground(200,450,400,280);
+
+   
+    sling=new Slingshot(bird.body,{x:300,y:150});
+
+   
 }
 
 function draw(){
-    background(0);
-    Engine.update(myEngine);
    
+    if (bg)
+    background(bg);
+    stroke("white");
+    strokeWeight(3);
+    textSize(30);
+    text("Score:"+score,1000,30)
+    
+    
+    Engine.update(myEngine);
+    console.log(score) 
     box.display();
     box2.display();
+    box3.display();
+    box4.display();
     pig.display();
+    pig2.display();
+    box5.display();
     bird.display();
+    log1.display();
+    log2.display();
+    log3.display();
+    log4.display();
    ground.display();
+   sling.display();
+   platform.display();
+   pig.scoreinc();
+   pig2.scoreinc();
 } 
+
+function mouseDragged(){
+    if (gameState !== "launched"){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+    }
+}
+
+function mouseReleased(){
+    sling.fly();
+    gameState="launched";
+}
+
+function keyPressed(){
+    if (keyCode===32 && bird.body.speed<1){
+        bird.trajectory=[];
+        Matter.Body.setPosition(bird.body,{x:300,y:150})
+        sling.attach(bird.body);
+    }
+}
+
+async function getTime(){
+
+  var response= await fetch("http://worldclockapi.com/api/json/est/now");
+  var responseJSON= await response.json();
+  var dt = responseJSON.currentDateTime;
+  var hour = dt.slice(11,13);
+ // console.log(responseJSON);
+
+ if (hour<=06 && hour>=19){
+     back="sprites/bg.png";
+ }
+ else{
+     back="sprites/bg2.jpg";
+ }
+
+ bg=loadImage(back);
+}
